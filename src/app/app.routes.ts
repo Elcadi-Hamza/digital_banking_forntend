cd30e6a55a35ffc25f64840a3be1507c304a77f6
elcadiHamza <email_du_compte_rick679>
2026-05-29 18:01:31 +0100

    add the routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CustomersComponent} from "./customers/customers";
+import {AccountsComponent} from "./accounts/accounts";
+import {NewCustomerComponent} from "./new-customer/new-customer";
+import {CustomerAccountsComponent} from "./customer-accounts/customer-accounts";
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path :"customers", component : CustomersComponent},
+  { path :"accounts", component : AccountsComponent},
+  { path :"new-customer", component : NewCustomerComponent},
+  { path :"customer-accounts/:id", component : CustomerAccountsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/customer-accounts/customer-accounts.ts
+++ src/app/customer-accounts/customer-accounts.ts
@@ -1,58 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {CustomerService} from "../services/customer.service";
-import {catchError, map, Observable, throwError} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Customer} from "../model/customer.model";
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  selector: 'app-customer-accounts',
+  templateUrl: './customer-accounts.component.html',
+  styleUrls: ['./customer-accounts.component.css']
 })
-export class CustomersComponent implements OnInit {
-  customers! : Observable<Array<Customer>>;
-  errorMessage!: string;
-  searchFormGroup : FormGroup | undefined;
-  constructor(private customerService : CustomerService, private fb : FormBuilder, private router : Router) { }
+export class CustomerAccountsComponent implements OnInit {
+  customerId! : string ;
+  customer! : Customer;
+  constructor(private route : ActivatedRoute, private router :Router) {
+    this.customer=this.router.getCurrentNavigation()?.extras.state as Customer;
+  }
 
   ngOnInit(): void {
-    this.searchFormGroup=this.fb.group({
-      keyword : this.fb.control("")
-    });
-    this.handleSearchCustomers();
-  }
-  handleSearchCustomers() {
-    let kw=this.searchFormGroup?.value.keyword;
-    this.customers=this.customerService.searchCustomers(kw).pipe(
-      catchError(err => {
-        this.errorMessage=err.message;
-        return throwError(err);
-      })
-    );
+    this.customerId = this.route.snapshot.params['id'];
+
   }
 
-  handleDeleteCustomer(c: Customer) {
-    let conf = confirm("Are you sure?");
-    if(!conf) return;
-    this.customerService.deleteCustomer(c.id).subscribe({
-      next : (resp) => {
-        this.customers=this.customers.pipe(
-          map(data=>{
-            let index=data.indexOf(c);
-            data.slice(index,1)
-            return data;
-          })
-        );
-      },
-      error : err => {
-        console.log(err);
-      }
-    })
-  }
-
-  handleCustomerAccounts(customer: Customer) {
-    this.router.navigateByUrl("/customer-accounts/"+customer.id,{state :customer});
-  }
 }
