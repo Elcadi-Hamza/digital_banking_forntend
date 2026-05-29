d5a34c699b859955b946dae61e511c22156f7159
elcadiHamza <email_du_compte_rick679>
2026-05-29 17:48:13 +0100

    add customer accouns component

---
+++ src/app/customer-accounts/customer-accounts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-accounts',
+  imports: [],
+  templateUrl: './customer-accounts.html',
+  styleUrls: ['./customer-accounts.css'],
+})
+export class CustomerAccountsComponent {}
